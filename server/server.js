60013bf1bd0335e458d1f6109a35e29e30885497
Abhinav Walde <abhinav204walde@gmail.com>
2026-06-18 20:31:17 +0530

    Update backend URL to Render production deployment

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'https://intell-meet-phi.vercel.app',
   credentials: true
 }));
 app.use(helmet());
